ad4cab4712a468b3885eedd48533e1dd096c133c
PFBregon <bregon@gmail.com>
2025-01-27 17:48:17 +0100

    index movement

--- pick_up_color/vite.config.js
+++ pick_up_color/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: ./Pick_up_color/,
   plugins: [react()],
+  base: "/Pick_up_color/",
 })
